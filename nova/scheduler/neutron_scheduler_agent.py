a94b683fd006560aef8bf115c8b6d92d84084fd9
teddycow <fluffgohei@hotmail.com>
2014-05-19 16:49:05 +0200

    new queue (pika alternative)

--- nova/scheduler/neutron_scheduler_agent.py
+++ nova/scheduler/neutron_scheduler_agent.py
@@ -5,15 +5,18 @@ from nova.openstack.common import jsonutils
 from nova import rpc
 import copy
 from nova.objects import base as objects_base
+import pika
+
 
 class NeutronScheduler(object):
     def __init__(self, topic):
         super(NeutronScheduler, self).__init__()
-        self.topic = topic
-        target = messaging.Target(topic=self.topic, version='1.0', exchange='nova')
+        self.topic = 'scheduler'
+        target = messaging.Target(topic=self.topic, version='1.0')
         serializer = objects_base.NovaObjectSerializer()
         self.client = rpc.get_client(target, serializer=serializer)
         self.host_dict = []
+      #  main()
 
     def neutron_scheduler(self, hosts, chain_name, weights, instance):
         """Make a remote process call to use Neutron's filter scheduler."""
@@ -31,3 +34,18 @@ class NeutronScheduler(object):
                            hosts=jsonutils.to_primitive(self.host_dict),
                            chain_name=chain_name,
                            weight_functions=weights)
+
+
+def main():
+
+    credentials = pika.PlainCredentials('guest', 'simple')
+    parameters = pika.ConnectionParameters('localhost',
+                                       5672,
+                                       '/',
+                                       credentials)
+
+    connection = pika.BlockingConnection(parameters)
+    channel = connection.channel()
+    channel.queue_bind(exchange='nova',
+    queue='topic.filter_scheduler',
+    routing_key='topic.filter_scheduler')
