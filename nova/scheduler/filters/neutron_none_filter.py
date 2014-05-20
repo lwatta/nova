3c846f30b4854f279f486253eb027ad87a30e345
teddycow <fluffgohei@hotmail.com>
2014-05-20 15:14:03 +0200

    Small fix

--- nova/scheduler/filters/neutron_none_filter.py
+++ nova/scheduler/filters/neutron_none_filter.py
@@ -5,22 +5,23 @@ from nova.scheduler.neutron_scheduler_agent import NeutronScheduler
 from nova.scheduler.host_manager import HostState
 from nova import exception
 
+
 class HostS(HostState):
     def __init__(self, adict):
         self.__dict__.update(adict)
+
 
 def to_object(adict):
     return HostS(adict)
 
 
-class NeutronFilter(filters.BaseHostFilter):
+class NeutronNoneFilter(filters.BaseHostFilter):
 
     def filter_all(self, filter_obj_list, filter_properties):
 
         chain_name = 'no_filter'
         weights = None
         instance = filter_properties
-        exchange = 'neutron'
         topic = 'topic.filter_scheduler'
         ns = NeutronScheduler(topic)
         hosts = ns.neutron_scheduler(filter_obj_list, chain_name, weights, instance)
