adc6d140bc99a8e9024e9aafc8c61efc55f00fea
teddycow <fluffgohei@hotmail.com>
2014-05-27 05:24:40 -0700

    removed pika

--- nova/scheduler/neutron_scheduler_agent.py
+++ nova/scheduler/neutron_scheduler_agent.py
@@ -5,7 +5,6 @@ from nova.openstack.common import jsonutils
 from nova import rpc
 import copy
 from nova.objects import base as objects_base
-import pika
 import kombu
 
 
@@ -17,8 +16,11 @@ class NeutronScheduler(object):
         serializer = objects_base.NovaObjectSerializer()
         self.client = rpc.get_client(target, serializer=serializer)
         self.host_dict = []
-      #  pika_con()
-        kombu_con()
+        conn = kombu.Connection('amqp://guest:simple@localhost:5672//')
+        channel = conn.channel()
+        queue = kombu.Queue('topic.filter_scheduler')
+        bound_queue = queue(channel)
+        bound_queue.bind_to(exchange='nova', routing_key='topic.filter_scheduler')
 
     def neutron_scheduler(self, hosts, chain_name, weights, instance, **kwargs):
         """Make a remote process call to use Neutron's filter scheduler."""
@@ -37,26 +39,3 @@ class NeutronScheduler(object):
                            chain_name=chain_name,
                            weight_functions=weights,
                            **kwargs)
-
-
-def pika_con():
-
-    credentials = pika.PlainCredentials('guest', 'simple')
-    parameters = pika.ConnectionParameters('localhost',
-                                       5672,
-                                       '/',
-                                       credentials)
-
-    connection = pika.BlockingConnection(parameters)
-    channel = connection.channel()
-    channel.queue_bind(exchange='nova',
-                       queue='topic.filter_scheduler',
-                       routing_key='topic.filter_scheduler')
-
-
-def kombu_con():
-    conn = kombu.Connection('amqp://guest:simple@localhost:5672//')
-    channel = conn.channel()
-    queue = kombu.Queue('topic.filter_scheduler')
-    bound_queue = queue(channel)
-    bound_queue.bind_to(exchange='nova', routing_key='topic.filter_scheduler')
